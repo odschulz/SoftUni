d597c044c2b963cf4da6f61d5e91bc5adc8a59cf
odschulz <grg.marchev@gmail.com>
2015-05-24 18:51:43 +0300

    Added a loading function and navigation icons

--- Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/app.js
+++ Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('socialNetworkApp', ['ngResource', 'ngRoute', 'ngStorage', 'ui-notification']);
+var app = angular.module('socialNetworkApp', ['ngResource', 'ngRoute', 'ngStorage', 'angularSpinner', 'ui-notification']);
 
 app.config(['$routeProvider',
     function($routeProvider) {
@@ -7,11 +7,33 @@ app.config(['$routeProvider',
                 templateUrl: 'templates/home.html',
                 controller: 'MainController'
             })
+            .when('/settings/edit/profile/', {
+                templateUrl: 'templates/home.html',
+                controller: 'MainController'
+            })
+            .when('/settings/edit/password/', {
+                templateUrl: 'templates/home.html',
+                controller: 'MainController'
+            })
             .otherwise({
                 redirectTo: '/'
             });
     }
 ]);
+
+app.config(['usSpinnerConfigProvider', function (usSpinnerConfigProvider) {
+    usSpinnerConfigProvider.setDefaults({
+        lines: 7, // The number of lines to draw
+        length: 1, // The length of each line
+        width: 21, // The line thickness
+        radius: 23, // The radius of the inner circle
+        corners: 0.6, // Corner roundness (0..1)
+        rotate: 90, // The rotation offset
+        color: '#66AFE9', // #rgb or #rrggbb or array of colors
+        speed: 0.5, // Rounds per second
+        trail: 10 // Afterglow percentage
+    });
+}]);
 
 app.constant({
     'BASE_SERVICE_URL':'http://softuni-social-network.azurewebsites.net/api/'
--- Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/controllers/userController.js
+++ Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/controllers/userController.js
@@ -1,4 +1,4 @@
-app.controller('UserController', ['$scope', 'authentication', 'Notification', 'userData', function ($scope, authentication, Notification, userData) {
+app.controller('UserController', ['$scope', 'authentication', 'Notification', 'userData', 'usSpinnerService', function ($scope, authentication, Notification, userData, usSpinnerService) {
     $scope.isLogged = function () {
         return authentication.isLogged();
     };
@@ -6,9 +6,12 @@ app.controller('UserController', ['$scope', 'authentication', 'Notification', 'u
     $scope.login = function () {
         var user = $scope.user;
 
+        usSpinnerService.spin('spinner-1');
+
         userData.login($scope.user)
             .$promise
             .then(function (data) {
+                usSpinnerService.stop('spinner-1');
                 authentication.saveUser(data);
             }, function (error) {
                 Notification.error({message: error.data.error_description, delay: 2000});
@@ -18,9 +21,12 @@ app.controller('UserController', ['$scope', 'authentication', 'Notification', 'u
     $scope.register = function () {
         var registerUserData = $scope.registerUserData;
 
+        usSpinnerService.spin('spinner-1');
+
         userData.register(registerUserData)
             .$promise
             .then(function (data) {
+                usSpinnerService.stop('spinner-1');
                 authentication.saveUser(data);
             }, function (error) {
                 Notification.error({message: error.data.error_description, delay: 2000});
@@ -28,9 +34,12 @@ app.controller('UserController', ['$scope', 'authentication', 'Notification', 'u
     };
 
     $scope.logout = function () {
+        usSpinnerService.spin('spinner-1');
+
         userData.logout()
             .$promise
             .then(function (data) {
+                usSpinnerService.stop('spinner-1');
                 authentication.removeUser();
             }, function (error) {
                 Notification.error({message: error.data.error_description, delay: 2000});
