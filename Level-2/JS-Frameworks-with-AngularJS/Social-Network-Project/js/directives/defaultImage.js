e20ef8454ded8e5b8e8f1548ca3dad1629d2e6d5
odschulz <grg.marchev@gmail.com>
2015-05-29 19:57:17 +0300

    Fix
    Adjusted the breakpoint for the navbar collapse menu

--- Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/directives/defaultImage.js
+++ Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/directives/defaultImage.js
@@ -1,11 +1,11 @@
 app.directive('defaultImage', function() {
     return {
         link: function(scope, element, attrs) {
-            element.bind('error', function() {
-                if (attrs.src != attrs.defaultImage) {
-                    attrs.$set('src', attrs.defaultImage);
-                }
-            });
+            //element.bind('error', function() {
+            //    if (attrs.src != attrs.defaultImage) {
+            //        attrs.$set('src', attrs.defaultImage);
+            //    }
+            //});
 
             attrs.$observe('ngSrc', function(value) {
                 if (!value && attrs.defaultImage) {
