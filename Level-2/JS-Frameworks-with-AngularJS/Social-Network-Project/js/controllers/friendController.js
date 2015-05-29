95d033bb2130bfea8cac30993847702a52adc740
odschulz <grg.marchev@gmail.com>
2015-05-30 00:13:35 +0300

    Added functionality:
    Implemented friends preview on home with up to 6 friends and a total
    number with link, leading to friends list page.

--- Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/controllers/friendController.js
+++ Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/controllers/friendController.js
@@ -6,8 +6,37 @@ app.controller(
         '$timeout',
         'authentication',
         'Notification',
+        'friendData',
         'userData',
         'usSpinnerService',
-        function ($scope, $location, $timeout, authentication, Notification, userData, usSpinnerService) {
+        'FRIENDS_PREVIEW_COUNT',
+        function ($scope, $location, $timeout, authentication, Notification, friendData, userData, usSpinnerService, FRIENDS_PREVIEW_COUNT) {
+            $scope.getMyFriendsPreviewData = function () {
+                usSpinnerService.spin('spinner-1');
 
+                friendData.getMyFriendsPreviewData(getAuthenticationHeaders())
+                    .$promise
+                    .then(function (data) {
+                        var friendsPreviewData;
+                        usSpinnerService.stop('spinner-1');
+
+                        if (data.totalCount > 6) {
+                            friendsPreviewData = data.friends.slice(0, 6);
+                            console.log(friendsPreviewData);
+                        } else {
+                            friendsPreviewData = data.friends;
+                        }
+
+                        friendsPreviewData.totalCount = data.totalCount;
+                        $scope.friendsPreviewData = friendsPreviewData;
+                    }, function (error) {
+                        usSpinnerService.stop('spinner-1');
+                        Notification.error({message: 'Could not retriece friends data!', delay: 4000});
+                        console.log(error);
+                    });
+            };
+
+            function getAuthenticationHeaders() {
+                return authentication.getHeaders();
+            }
         }]);
--- Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/services/data/friendData.js
+++ Level-2/JS-Frameworks-with-AngularJS/Social-Network-Project/js/services/data/friendData.js
@@ -1,8 +1,21 @@
 app.factory('friendData', ['$resource', 'BASE_SERVICE_URL',function ($resource, BASE_SERVICE_URL) {
 
+    function getMyFriendsPreviewData(authenticationHeaders) {
+
+        return $resource(
+            BASE_SERVICE_URL + 'me/friends/preview',
+            null,
+            {
+                'get': {
+                    method: 'GET',
+                    headers: authenticationHeaders
+                }
+            })
+            .get();
+    }
 
     return {
-
+        getMyFriendsPreviewData: getMyFriendsPreviewData
     }
 }]);
 
